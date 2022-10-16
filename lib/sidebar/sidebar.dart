f103f05fff7acf434a348857822907df180d9066
sarikhin <sarikhinlovestory@gmail.com>
2022-10-16 22:13:04 +0700

    first commit

---
+++ lib/sidebar/sidebar.dart
@@ -0,0 +1,8 @@
+import 'package:sidebarx/sidebarx.dart';
+import 'package:flutter/material.dart';
+
+List<SidebarXItem> sidebar = [
+  SidebarXItem(label: 'Menu', icon: Icons.mail, onTap: () {}),
+  SidebarXItem(label: 'Menu', icon: Icons.mail, onTap: () {}),
+  SidebarXItem(label: 'Menu', icon: Icons.mail, onTap: () {})
+];
